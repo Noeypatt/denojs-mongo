5f128a6689fac199a45b9743b53eaa83fdec2046
Noeypatt <patnarin.s@mapboss.co.th>
2020-05-17 15:13:15 +0700

    Deno/get/gets

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const env = Deno.env.toObject()
 const PORT = env.PORT || 8000
 const HOST = env.HOST || '127.0.0.1'
 
+let dogs = [
+    {
+        name: 'Roger',
+        age: 8,
+    },
+    {
+        name: 'Syd',
+        age: 7,
+    },
+]
 
 const router = new Router();
 
@@ -12,14 +22,31 @@ router.get('/', context => {
     context.response.body = "Hello Deno 🦕"
 })
 
-router.post('/', context => {
-    context.response.body = "POST request"
-})
+router
+    .get('/dogs', context => {
+        context.response.body = dogs
+    })
+    .get('/dog/:name', context => {
+        const name = context.params.name
+        const dog = dogs.filter((dog) => dog.name === name)
+
+        if (dog.length > 0) {
+            context.response.body = dog
+        }
+        else {
+            context.response.body = `${name} Not found`
+        }
+
+    })
+
+
+
 
 const app = new Application();
-
 app.use(router.routes())
 app.use(router.allowedMethods())
 
 const server = app.listen(`${HOST}:${PORT}`);
 console.log(`http://${HOST}:${PORT}`);
+
+
