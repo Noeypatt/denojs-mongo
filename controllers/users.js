0950fdb3ab3b2c6d9b73101528292f3e14a04fc8
noeypatt <patnarin241240@gmail.com>
2020-05-17 21:46:40 +0700

    Deno/get-one v2

--- controllers/users.js
+++ controllers/users.js
@@ -13,7 +13,21 @@ let dogs = [
     },
 ]
 
-export const fetchAllEmployees = (context) => {
+export const fetchAllDog = (context) => {
     context.response.status = 200
     context.response.body = dogs
+}
+
+export const fetchOneDog = (context) => {
+    const name = context.params.name
+    const dog = dogs.filter((dog) => dog.name === name)
+
+    if (dog.length > 0) {
+        context.response.status = 200
+        context.response.body = dog
+    }
+    else {
+        context.response.status = 404
+        context.response.body = { msg: `Cannot find dog name: ${name}` }
+    }
 }
