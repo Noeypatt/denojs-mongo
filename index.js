e4b651c5f8d630d04572b27c4abe5e421fab467e
noeypatt <patnarin241240@gmail.com>
2020-05-17 23:50:14 +0700

    Deno/routes

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-import { Application, Router } from 'https://deno.land/x/oak/mod.ts'
+import { Application } from 'https://deno.land/x/oak/mod.ts'
 import "https://deno.land/x/dotenv/load.ts";
-import { fetchAllDogs, fetchOneDog, createDog, updateDog, deleteDog } from './controllers/users.js'
+import router from './routes/user.js'
 
 const env = Deno.env.toObject()
 const PORT = env.PORT || 8000
 const HOST = env.HOST || '127.0.0.1'
 
-const router = new Router();
-
-router.get('/', context => {
-    context.response.body = "Hello Deno 🦕"
-})
-
-router
-    .get('/dogs', fetchAllDogs)
-    .get('/dog/:name', fetchOneDog)
-    .post('/dogs', createDog)
-    .put('/dog/:name', updateDog)
-    .delete('/dog/:name', deleteDog)
-
 const app = new Application();
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-const server = app.listen(`${HOST}:${PORT}`);
+app.listen(`${HOST}:${PORT}`);
 console.log(`http://${HOST}:${PORT}`);
 
 
---
+++ routes/user.js
@@ -0,0 +1,19 @@
+import { Router } from 'https://deno.land/x/oak/mod.ts'
+import { fetchAllDogs, fetchOneDog, createDog, updateDog, deleteDog } from '../controllers/users.js'
+
+
+const router = new Router();
+
+router.get('/', context => {
+    context.response.body = "Hello Deno 🦕"
+})
+
+router
+    .get('/dogs', fetchAllDogs)
+    .get('/dog/:name', fetchOneDog)
+    .post('/dogs', createDog)
+    .put('/dog/:name', updateDog)
+    .delete('/dog/:name', deleteDog)
+
+
+    export default router
