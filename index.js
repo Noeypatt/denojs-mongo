859570ca6592be1393f6045fedb998e28abd39da
Noeypatt <patnarin.s@mapboss.co.th>
2020-05-17 16:43:03 +0700

    Deno/put

--- index.js
+++ index.js
@@ -48,16 +48,36 @@ router
         const body = await context.request.body()
         const dog = body.value
 
-        if (dog.name != undefined  && !dog.age != undefined) {
+        if (dog.name != undefined && !dog.age != undefined) {
             dogs.push(dog)
             context.response.body = { msg: 'Dog created!' }
             context.response.status = 200
         }
-        else{
+        else {
             context.response.body = { msg: 'Error' }
             context.response.status = 400
         }
 
+    })
+    .put('/dog/:name', async context => {
+        const dogName = context.params.name
+        const temp = dogs.filter((dog) => dog.name === dogName)
+
+        const body = await context.request.body()
+        const dog = body.value
+
+        if (temp.length) {
+            temp[0].name = dog.name ? dog.name: temp[0].name
+            temp[0].age = dog.age ? dog.age: temp[0].age
+
+            context.response.body = { msg: 'Update!!!' }
+            context.response.status = 200
+        }
+
+        else {
+            response.status = 400
+            response.body = { msg: `Cannot find dog name: ${name}` }
+        }
     })
 
 
