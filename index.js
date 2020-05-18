de8305ff86d1f5f4383b8892f2b38f37c8e1211d
noeypatt <patnarin241240@gmail.com>
2020-05-18 16:20:33 +0700

    Deno :tada:

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts'
 import "https://deno.land/x/dotenv/load.ts";
-// import routerDog from './routes/dogs.js'
-import routerDino from './routes/dino.js'
+import routerDog from './routes/dogs.js'
 
 const env = Deno.env.toObject()
 const PORT = env.PORT || 8000
@@ -9,13 +8,8 @@ const HOST = env.HOST || '127.0.0.1'
 
 const app = new Application();
 
-//Dog
-// app.use(routerDog.routes());
-// app.use(routerDog.allowedMethods());
-
-//Dino
-app.use(routerDino.routes());
-app.use(routerDino.allowedMethods());
+app.use(routerDog.routes());
+app.use(routerDog.allowedMethods());
 
 app.listen(`${HOST}:${PORT}`);
 console.log(`http://${HOST}:${PORT}`);
