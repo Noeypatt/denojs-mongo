a5f1917e093656b247efd719ddec2d8e199a3394
noeypatt <patnarin241240@gmail.com>
2020-05-18 15:34:49 +0700

    Deno/dinosaur/routes

---
+++ routes/dino.js
@@ -0,0 +1,15 @@
+import { Router } from 'https://deno.land/x/oak/mod.ts'
+import { fetchAllDino, fetchOneDino, amountDino} from '../controllers/dino.js'
+
+const router = new Router();
+
+router.get('/', context => {
+    context.response.body = "Hello Dog 🐕"
+})
+
+router
+    .get('/dinos', fetchAllDino)
+    .get('/dino/:name', fetchOneDino)
+    .get('/dinos/count', amountDino)
+
+export default router
